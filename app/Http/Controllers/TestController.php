fbb3ab7632a9a348a57c453b626d381e67f50199
qinqin <qq@2048.net>
2020-12-03 13:42:21 +0800

    demo test

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Http\Models\TestDemo;
+
+class ShareController extends Controller
+{
+	public function test(){
+
+	    $test   =   new TestDemo();
+
+        $test->firstOrCreate();
+
+    }
+
+
+
+
+}
---
+++ app/Http/Models/TestDemo.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TestDemo extends Model
+{
+	protected $table = 'tbl_test';
+	protected $connection = 'test';
+	protected $guarded = [];
+
+
+
+}
